706f1033072050201bb958f3d94b1dd9f5fc19a4
edgar <edgar@163.com>
2023-07-13 00:18:01 +0800

    get meta info from model in onnxruntime mode

--- yolov8_test.py
+++ yolov8_test.py
@@ -43,11 +43,20 @@ class YOLOV8_Process(Process):
         elif imageInfo["deploy"] == "onnxruntime":
             self.mode = onnxruntime.InferenceSession("yolov8n.onnx",
                                                     providers=['CUDAExecutionProvider'])
+            
+
+            
+            output_names = [x.name for x in self.mode.get_outputs()]
+            log.debug(output_names)
+            meta = self.mode.get_modelmeta().custom_metadata_map  # metadata
+            self.names = eval(meta['names'])
+    
+            # return
         elif imageInfo["deploy"] == "tensorrt":
             pass
         elif imageInfo["deploy"] == "opencv":
             pass
-
+        
         capture = cv.VideoCapture(imagePath)
         while True:
             _, frame = capture.read()
@@ -102,6 +111,9 @@ class YOLOV8_Process(Process):
                 cv.rectangle(frame, box, color, 2)
                 cv.rectangle(frame, (box[0], box[1] - 20), (box[0] + box[2], box[1]), color, -1)
                 cv.putText(frame, self.class_list[class_ids[index]], (box[0], box[1] - 10), cv.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 0))
+                # cv.putText(frame, self.names[(class_ids[index])], (box[0], box[1] - 10), cv.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 0))
+                # log.debug(self.class_list[(class_ids[index])])
+                # log.debug(self.names[(class_ids[index])])
             end = time.time()
             inf_end = end - start
             fps = 1 / inf_end
@@ -123,77 +135,3 @@ class YOLOV8_Process(Process):
         result[0:row, 0:col] = frame
         return result
 
-
-
-
-
-
-
-# class_list = load_classes()
-# colors = [(255, 255, 0), (0, 255, 0), (0, 255, 255), (255, 0, 0)]
-
-# # initialize the onnxruntime session by loading model in CUDA support
-# session = onnxruntime.InferenceSession("yolov8n.onnx",
-#                                                     providers=['CUDAExecutionProvider'])
-
-# capture = cv.VideoCapture("./lxyy.mp4")
-# while True:
-#     _, frame = capture.read()
-#     if frame is None:
-#         print("End of stream")
-#         break
-#     bgr = format_yolov8(frame)
-#     img_h, img_w, img_c = bgr.shape
-
-#     start = time.time()
-#     image = cv.dnn.blobFromImage(bgr, 1 / 255.0, (640, 640), swapRB=True, crop=False)
-
-#     # onnxruntime inference
-#     ort_inputs = {session.get_inputs()[0].name: image}
-#     res = session.run(None, ort_inputs)[0]
-
-#     # matrix transpose from 1x84x8400 => 8400x84
-#     rows = np.squeeze(res, 0).T
-
-#     # post-process
-#     class_ids = []
-#     confidences = []
-#     boxes = []
-#     x_factor = img_w / 640
-#     y_factor = img_h / 640
-
-#     for r in range(rows.shape[0]):
-#         row = rows[r]
-#         classes_scores = row[4:]
-#         _, _, _, max_indx = cv.minMaxLoc(classes_scores)
-#         class_id = max_indx[1]
-#         if (classes_scores[class_id] > .25):
-#             confidences.append(classes_scores[class_id])
-#             class_ids.append(class_id)
-#             x, y, w, h = row[0].item(), row[1].item(), row[2].item(), row[3].item()
-#             left = int((x - 0.5 * w) * x_factor)
-#             top = int((y - 0.5 * h) * y_factor)
-#             width = int(w * x_factor)
-#             height = int(h * y_factor)
-#             box = np.array([left, top, width, height])
-#             boxes.append(box)
-
-#     # NMS
-#     indexes = cv.dnn.NMSBoxes(boxes, confidences, 0.25, 0.45)
-#     for index in indexes:
-#         box = boxes[index]
-#         color = colors[int(class_ids[index]) % len(colors)]
-#         cv.rectangle(frame, box, color, 2)
-#         cv.rectangle(frame, (box[0], box[1] - 20), (box[0] + box[2], box[1]), color, -1)
-#         cv.putText(frame, class_list[class_ids[index]], (box[0], box[1] - 10), cv.FONT_HERSHEY_SIMPLEX, .5, (0, 0, 0))
-#     end = time.time()
-#     inf_end = end - start
-#     fps = 1 / inf_end
-#     fps_label = "FPS: %.2f" % fps
-#     cv.putText(frame, fps_label, (20, 45), cv.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
-
-#     cv.imshow("YOLOv8+onnxruntime Object Detection", frame)
-#     cc = cv.waitKey(1)
-#     if cc == 27:
-#         break
-# cv.destroyAllWindows()
